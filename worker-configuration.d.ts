1e291956a910acb6d68df70d76a8949397f3ee76
Wei Sun <sagasw@gmail.com>
2025-12-05 05:24:40 +0000

    add

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -8,6 +8,8 @@ declare namespace Cloudflare {
 	interface Env {
 		ZHIPU_API_KEY?: string;
         ZAI_MODEL: string;
+        SILICONFLOW_API_KEY?: string;
+        SILICONFLOW_MODEL: string;
         
 	}
 }
